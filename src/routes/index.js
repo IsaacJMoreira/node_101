0262ed400cf2e4b814c39a9845a8715694eaaa97
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-06-24 18:04:24 -0300

    Metdos get funcionando, agora posso fazer tudo!!!

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,4 +15,12 @@ routes.get('/listar', (req, res)=>{
 
 });
 
+routes.get('/:number1/:number2/add', (req, res)=>{
+    let num1 = req.params['number1'];
+    let num2 = req.params['number2'];
+    let result = (Number(num1) + Number(num2)).toString();
+    res.send(result);
+    console.log("Calculei: " + num1 +" + " + num2 + " = " + result);
+});
+
 module.exports = routes;
